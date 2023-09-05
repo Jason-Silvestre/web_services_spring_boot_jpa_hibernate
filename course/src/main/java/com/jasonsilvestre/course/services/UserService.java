7b01f0485fbebd3614f1a0c086f2965046c0af51
Jason Silvestre <jasonsilvestre43@yahoo.com>
2023-09-05 16:10:02 -0300

    User update

--- course/src/main/java/com/jasonsilvestre/course/services/UserService.java
+++ course/src/main/java/com/jasonsilvestre/course/services/UserService.java
@@ -2,6 +2,8 @@ package com.jasonsilvestre.course.services;
 
 import com.jasonsilvestre.course.entities.User;
 import com.jasonsilvestre.course.repositories.UserRepository;
+import com.jasonsilvestre.course.resources.exceptions.ResourceExceptionHandler;
+import com.jasonsilvestre.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
